11edb298b47c67e86a01f2e010d0a20ef005129f
Diego Quan <dquan@delicatessen502.com>
2026-04-07 14:58:53 -0600

    Prueba de guardar en la BD

--- visa-app/backend/index.js
+++ visa-app/backend/index.js
@@ -1,12 +1,52 @@
-const express = require('express');
+const express = require("express");
+const { Pool } = require("pg");
+
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Backend funcionando');
+// conexión a PostgreSQL
+const pool = new Pool({
+  user: "postgres",
+  host: "127.0.0.1",
+  database: "visa_db",
+  password: "postgres",
+  port: 5433,
 });
 
+// probar conexión
+pool.connect()
+  .then(() => console.log("Conectado a PostgreSQL"))
+  .catch(err => console.error("Error conexión:", err));
+
+// endpoint de prueba
+app.get("/", (req, res) => {
+  res.send("Backend funcionando");
+});
+
+// endpoint registro REAL
+app.post("/register", async (req, res) => {
+  const { nombre, correo, contrasena } = req.body;
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO usuario(nombre, correo, contrasena) VALUES($1,$2,$3) RETURNING *",
+      [nombre, correo, contrasena]
+    );
+
+    res.json({
+      message: "Usuario guardado en BD",
+      data: result.rows[0],
+    });
+
+  } catch (error) {
+    console.error("ERROR DETALLADO:", error.message);
+    res.status(500).json({ error: error.message });
+    }
+});
+
+// iniciar servidor
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
